c4b246bd9cfe2456fbd2cd8ea8a9c6bc609588d2
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2016-08-20 04:01:52 +0900

    ﻿tests/loop_test を追加

---
+++ tests/bnet/loop_test.cc
@@ -0,0 +1,88 @@
+
+/// @file BnBuilderTest.cc
+/// @brief BnBuilderTest の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2016 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "ym/BnBuilder.h"
+#include "ym/BnBlifReader.h"
+#include "ym/BnIscas89Reader.h"
+#include "ym/Iscas89Parser.h"
+
+
+BEGIN_NAMESPACE_YM
+
+void
+blif_loop_test(ymuint loop_count,
+	       const string& filename)
+{
+  for (ymuint i = 0; i < loop_count; ++ i) {
+    BnBuilder builder;
+    BnBlifReader reader;
+    reader.read(builder, filename);
+  }
+}
+
+void
+iscas89_loop_test(ymuint loop_count,
+		  const string& filename)
+{
+  for (ymuint i = 0; i < loop_count; ++ i) {
+    BnBuilder builder;
+    BnIscas89Reader reader;
+    reader.read(builder, filename);
+  }
+}
+
+void
+iscas89parser_loop_test(ymuint loop_count,
+		  const string& filename)
+{
+  for (ymuint i = 0; i < loop_count; ++ i) {
+    Iscas89Parser parser;
+    parser.read(filename);
+  }
+}
+
+void
+usage(char* progname)
+{
+  cerr << "USAGE: " << progname << " <blif|iscas89> <# of loop counts> <iscas89 filename>" << endl;
+}
+
+END_NAMESPACE_YM
+
+int
+main(int argc,
+     char** argv)
+{
+  using namespace std;
+  using namespace nsYm;
+
+  if ( argc != 4 ) {
+    usage(argv[0]);
+    return 1;
+  }
+
+  int loop_count = atoi(argv[2]);
+  string filename = argv[3];
+
+  if ( strcmp(argv[1], "blif") == 0 ) {
+    blif_loop_test(loop_count, filename);
+  }
+  else if ( strcmp(argv[1], "iscas89") == 0 ) {
+    iscas89_loop_test(loop_count, filename);
+  }
+  else if ( strcmp(argv[1], "iscas89parser") == 0 ) {
+    iscas89parser_loop_test(loop_count, filename);
+  }
+  else {
+    usage(argv[0]);
+    return 1;
+  }
+
+  return 0;
+}
