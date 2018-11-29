28b435277ce0e262338dbd31b86712f2b2e93f3e
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-11-29 16:34:31 +0900

    BnBlifReaderTest.cc, BnIscas89ReaderTest.cc: 出力フォーマットを指定できるようにした．

--- test-programs/bnet/BnBlifReaderTest.cc
+++ test-programs/bnet/BnBlifReaderTest.cc
@@ -16,21 +16,40 @@ int
 BnBlifReaderTest(int argc,
 		 char** argv)
 {
-  if ( argc != 2 ) {
-    cerr << "USAGE: BnBlifReaderTest <blif-filename>" << endl;
+  bool blif = false;
+  bool iscas89 = false;
+
+  int argbase = 1;
+  if ( strcmp(argv[argbase], "--blif") == 0 ) {
+    blif = true;
+    ++ argbase;
+  }
+  else if ( strcmp(argv[argbase], "--iscas89") == 0 ) {
+    iscas89 = true;
+    ++ argbase;
+  }
+
+  if ( argc - argbase != 1 ) {
+    cerr << "USAGE: BnBlifReaderTest [--blif|--iscas89] <iscas89-filename>" << endl;
     return -1;
   }
 
-  string filename = argv[1];
+  string filename = argv[argbase];
   BnNetwork network = BnNetwork::read_blif(filename);
   if ( network.node_num() == 0 ) {
     cerr << "read_blif(" << filename << ") failed" << endl;
     return -1;
   }
 
-  BnNetwork network2(std::move(network));
-
-  network2.write(cout);
+  if ( blif ) {
+    network.write_blif(cout);
+  }
+  else if ( iscas89 ) {
+    network.write_iscas89(cout);
+  }
+  else {
+    network.write(cout);
+  }
 
   return 0;
 }
--- test-programs/bnet/BnIscas89ReaderTest.cc
+++ test-programs/bnet/BnIscas89ReaderTest.cc
@@ -14,21 +14,42 @@ BEGIN_NAMESPACE_YM
 
 int
 BnIscas89ReaderTest(int argc,
-		 char** argv)
+		    char** argv)
 {
-  if ( argc != 2 ) {
-    cerr << "USAGE: BnIscas89ReaderTest <iscas89-filename>" << endl;
+  bool blif = false;
+  bool iscas89 = false;
+
+  int argbase = 1;
+  if ( strcmp(argv[argbase], "--blif") == 0 ) {
+    blif = true;
+    ++ argbase;
+  }
+  else if ( strcmp(argv[argbase], "--iscas89") == 0 ) {
+    iscas89 = true;
+    ++ argbase;
+  }
+
+  if ( argc - argbase != 1 ) {
+    cerr << "USAGE: BnIscas89ReaderTest [--blif|--iscas89] <iscas89-filename>" << endl;
     return -1;
   }
 
-  string filename = argv[1];
+  string filename = argv[argbase];
   BnNetwork network = BnNetwork::read_iscas89(filename);
   if ( network.node_num() == 0 ) {
     cerr << "read_iscas89(" << filename << ") failed" << endl;
     return -1;
   }
 
-  network.write(cout);
+  if ( blif ) {
+    network.write_blif(cout);
+  }
+  else if ( iscas89 ) {
+    network.write_iscas89(cout);
+  }
+  else {
+    network.write(cout);
+  }
 
   return 0;
 }
