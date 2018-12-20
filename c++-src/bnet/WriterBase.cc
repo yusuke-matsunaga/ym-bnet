788fa55de16fc46968c891ccc30cf2b630a16138
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-12-20 13:32:23 +0900

    WriteBase.cc: 修正
    * 入力と出力が直接繋がっている場合の対応

--- c++-src/bnet/WriterBase.cc
+++ c++-src/bnet/WriterBase.cc
@@ -49,30 +49,37 @@ WriterBase::init_name_array(const string& prefix,
 
   // もともと与えられた名前があればそれを使う．
   // ただし重複のチェックを行う．
+
+  // まず外部入力の名前を登録する．
   for ( int id: mNetwork.input_id_list() ) {
     auto& node = mNetwork.node(id);
     if ( node.is_port_input() ) {
       reg_node_name(node, name_hash, name_mgr, node_list);
     }
   }
+  // 次に外部出力の名前を登録する．
   for ( int id: mNetwork.output_id_list() ) {
     auto& node = mNetwork.node(id);
     if ( node.is_port_output() ) {
       reg_node_name(node, name_hash, name_mgr, node_list);
     }
   }
+
+  // FFの出力を登録する．
   for ( int id: mNetwork.input_id_list() ) {
     auto& node = mNetwork.node(id);
     if ( !node.is_port_input() ) {
       reg_node_name(node, name_hash, name_mgr, node_list);
     }
   }
+  // FFの入力を登録する．
   for ( int id: mNetwork.output_id_list() ) {
     auto& node = mNetwork.node(id);
     if ( !node.is_port_output() ) {
       reg_node_name(node, name_hash, name_mgr, node_list);
     }
   }
+  // 論理ノードを登録する．
   for ( int id: mNetwork.logic_id_list() ) {
     auto& node = mNetwork.node(id);
     reg_node_name(node, name_hash, name_mgr, node_list);
@@ -89,10 +96,19 @@ WriterBase::init_name_array(const string& prefix,
     int id = mNetwork.output_id(i);
     int src_id = mNetwork.output_src_id(i);
     auto& node = mNetwork.node(id);
+    auto& src_node = mNetwork.node(src_id);
     if ( node.is_port_output() ) {
-      mNameArray[src_id] = mNameArray[id];
+      if ( src_node.is_port_input() ) {
+	// 外部入力が直接外部出力に繋がっている場合は例外
+	// 出力ノードの名前を入力ノードと同じにする．
+	mNameArray[id] = mNameArray[src_id];
+      }
+      else {
+	mNameArray[src_id] = mNameArray[id];
+      }
     }
     else if ( node.is_dff_input() ) {
+      // FFの入力の場合はファンインの名前にする．
       mNameArray[id] = mNameArray[src_id];
     }
   }
@@ -132,6 +148,7 @@ WriterBase::reg_node_name(const BnNode& node,
   string name = node.name();
   int id = node.id();
   if ( name == string() ) {
+    // 名前がなかった．
     node_list.push_back(id);
   }
   else if ( name_hash.check(name) ) {
